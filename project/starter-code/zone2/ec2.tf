4f26c5af9448875dd64f46442f5fea4bb76f6726
imjokerx87 <imjokerx87@gmail.com>
2022-11-18 13:57:37 +0700

    .

--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@ module "project_ec2" {
   instance_count     = var.instance_count
   name               = local.name
   account            = data.aws_caller_identity.current.account_id
-  aws_ami            = "ami-01af75542ec1570ae"
+  aws_ami            = "ami-06365514778ccd95b"
   private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
   public_subnet_ids  = data.terraform_remote_state.vpc.outputs.public_subnet_ids
   vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
